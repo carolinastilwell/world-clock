b25fceab6733e7cfbe9865fe3473e670d13e7233
carolinastilwell <carolinastilwell@gmail.com>
2025-05-09 15:43:08 +0100

    Installed moment

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+let londonElementDate = document.querySelector("#london .date");
+londonElementDate.innerHTML = moment()
+  .tz("Europe/London")
+  .format("Do of MMMM, YYYY");
 
+let londonElementTime = document.querySelector("#london .time");
+londonElementTime.innerHTML = moment().tz("Europe/London").format("hh:mm:ss");
+
+let londonElementUnit = document.querySelector("#london .time small");
+londonElementUnit.innerHTML = moment().tz("Europe/London").format("A");
