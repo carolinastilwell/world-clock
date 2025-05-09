9b113b231c1b4cb4c3b8f36e71c655188bdd8775
carolinastilwell <carolinastilwell@gmail.com>
2025-05-09 17:28:47 +0100

    Added JS functions for select

--- src/index.js
+++ src/index.js
@@ -18,5 +18,27 @@ function updateCities() {
   updateTime("sao-paulo", "America/Sao_Paulo");
   updateTime("toronto", "America/Toronto");
 }
-
 setInterval(updateCities, 1000);
+
+function updateCity(event) {
+  setInterval(function () {
+    let cityTimeZone = event.target.value;
+    let cityName = cityTimeZone.replace(/_/g, " ").split("/")[1];
+    let cityTime = moment().tz(cityTimeZone);
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+           <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("Do of MMMM, YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format("h:mm:ss [<small>]A[</small>]")}
+            </div>
+             </div>
+
+      `;
+  }, 1000);
+}
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", updateCity);
