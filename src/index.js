455352765fc20abc0f649a5d9f0ef5070bbf2573
carolinastilwell <carolinastilwell@gmail.com>
2025-05-12 10:23:16 +0100

    Added current location

--- src/index.js
+++ src/index.js
@@ -23,6 +23,9 @@ setInterval(updateCities, 1000);
 function updateCity(event) {
   setInterval(function () {
     let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current") {
+      cityTimeZone = moment.tz.guess();
+    }
     let cityName = cityTimeZone.replace(/_/g, " ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
